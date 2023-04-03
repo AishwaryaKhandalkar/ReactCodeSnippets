af810d7fd9ad126953684533354631f8c4017e8c
Aishwarya Khandalkar <50652448+AishwaryaKhandalkar@users.noreply.github.com>
2023-04-03 14:56:28 -0700

    Functional components & JSX

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 1. Creation of React project with CDN links
 
 2. Usage of createElement on React Object and createRoot on ReactDOM
+
+3. Usage of browserlist package
+
+4. Usage of Parcel & parcel-cache
+
+5. Write scripts
+
+
+
+/*
+
+Igniting app:
+
+1. Initailise npm : npm init
+2. install react & react dom : npm install react , npm install react-dom
+3. Install parcel : npm install -D parcel (-D stands for dev dependency)
+4. Ignite app with parcel: npx parcel index.html
+5. add browserslist: In package.json
+*/
--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-/* <div id="parent">
-        <div id="child">
-            <h1>Hello I'm heading 1</h1>
-            <h2>Hello I'm heading 2</h2>
-        </div>
-        <div id="child2">
-            <h1>Hello I'm heading 1</h1>
-            <h2>Hello I'm heading 2</h2>
-        </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+const head = React.createElement('h1', {}, "Hello I'm heading 1");
+
+
+//Syntax for components
+
+//Functional components
+const Component1 = () => {
+  return <h1>Component 1</h1>
+}
+
+const Component2 = function() {
+    return (
+        <h1>Component 2</h1>
+    )
+}
+
+// JSX
+const jsxHead = 
+    <div>
+        <h1>Hi JSX!</h1> 
+        <h2>Hello</h2>  
     </div>
-*/
-
-const nestedStructure = React.createElement(
-    'div', 
-    { id: 'parent'},
-    [ 
-        React.createElement( 'div', { id: 'child'}, [
-                React.createElement('h1', {}, "Hello I'm heading 1"), 
-                React.createElement('h2', {}, "Hello I'm heading 2")
-            ]
-        ), 
-        React.createElement( 'div', { id: 'child2'}, [
-                React.createElement('h1', {}, "Hello I'm heading 1"), 
-                React.createElement('h2', {}, "Hello I'm heading 2")
-            ]
-        )
-    ]
-)
 
 
-const heading = React.createElement("h1", { id: "head"}, "Hello World React!" ); // create Element takes 3 arguments
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedStructure);
+root.render(<><Component1 /><Component2 /></>);
+
+
+
